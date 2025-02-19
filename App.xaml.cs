b9ba2d0f692139224d2642fee0d9aa5ca2718dca
baczewskimateusz <mate.bacz@gmail.com>
2025-02-19 19:15:50 +0100

    LoginWindow

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,5 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using TODO_app.Views;
 
 namespace TODO_app
 {
@@ -9,6 +8,13 @@ namespace TODO_app
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            LoginWindow login = new LoginWindow();
+            login.Show();
+        }
     }
 
 }
